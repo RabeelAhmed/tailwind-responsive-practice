899416f772c92d5ab06272003f7882fbf48f9889
RabeelAhmed <rabeelsulehria3@gmail.com>
2024-10-31 22:04:01 +0500

    Add bento grid section.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 module.exports = {
   content: ['./index.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        red: {
+          200: '#FEB2B2', // Light red
+        },
+        purple: {
+          200: '#E9D8FD', // Light purple
+        },
+        yellow: {
+          200: '#FEFCBF', // Light yellow
+        },
+      },
+      gradientColorStops: theme => ({
+        'red-200': theme('colors.red.200'),
+        'purple-200': theme('colors.purple.200'),
+        'yellow-200': theme('colors.yellow.200'),
+      }),
+
+    },
   },
   plugins: [],
   corePlugins: {
